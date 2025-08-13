dddaa7a929dfb8ce2a03d56baf599cac81c43c94
João Pedro Omena dos Santos <220436756+jpomena@users.noreply.github.com>
2025-08-13 14:50:11 -0300

    Update __init__.py

--- src/__init__.py
+++ src/__init__.py
@@ -1,6 +1,7 @@
-from configs_window import ConfigsWindow  # noqa: F401
-from database import Database  # noqa: F401
-from main_window import MainWindow  # noqa: F401
-from report_window import ReportWindow  # noqa: F401
-from viagens_window import AbrirWindow  # noqa: F401
-from viagens_window import ApagarWindow  # noqa: F401
+from .configs_window import ConfigsWindow  # noqa: F401
+from .database import Database  # noqa: F401
+from .main_window import MainWindow  # noqa: F401
+from .report_window import ReportWindow  # noqa: F401
+from .viagens_window import AbrirWindow  # noqa: F401
+from .viagens_window import ApagarWindow  # noqa: F401
+
